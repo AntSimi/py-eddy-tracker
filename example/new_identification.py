7439dce3687b7b5f70911d4438cc43f1a741eac2
adelepoulle <adelepoulle@px-2059.px.cls.fr>
2018-03-09 17:55:31 +0100

    Add example with new class (draft)

---
+++ example/new_identification.py
@@ -0,0 +1,21 @@
+from py_eddy_tracker.dataset.grid import RegularGridDataset, UnRegularGridDataset
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+# h = UnRegularGridDataset('NEATL36_1d_grid2D_20140515-20140515_short.nc', 'nav_lon', 'nav_lat')
+# h.high_filter('sossheig', 20, 10)
+# h.add_uv('sossheig')
+# h.write('unregular.nc')
+
+# h = RegularGridDataset('msla_h_20170425T000000_20170425T000000.nc', 'NbLongitudes', 'NbLatitudes')
+# h.high_filter('Grid_0001', 10, 5)
+# h.eddy_identification('Grid_0001', 0.5)
+
+h = UnRegularGridDataset('unregular.nc', 'nav_lon', 'nav_lat')
+h.eddy_identification('sossheig')
+
+# h = RegularGridDataset('madt_h_20170425T000000_20170425T000000.nc', 'NbLongitudes', 'NbLatitudes')
+
+# h.add_uv('Grid_0001')
+# h.write('regular.nc')
+
