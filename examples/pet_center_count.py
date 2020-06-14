a10c425d34df153c0a69c77127f5eb17b723ffa7
A.delep <delepoulle.a@gmail.com>
2020-06-14 17:16:07 +0200

    add example

---
+++ examples/pet_center_count.py
@@ -0,0 +1,29 @@
+"""
+Count center
+======================
+
+"""
+
+from matplotlib import pyplot as plt
+from py_eddy_tracker.observations.tracking import TrackEddiesObservations
+import py_eddy_tracker_sample
+
+
+a = TrackEddiesObservations.load_file(py_eddy_tracker_sample.get_path("eddies_med_adt_allsat_dt2018/Anticyclonic.zarr"))
+c = TrackEddiesObservations.load_file(py_eddy_tracker_sample.get_path("eddies_med_adt_allsat_dt2018/Cyclonic.zarr"))
+a = a.merge(c)
+# Plot
+fig = plt.figure(figsize=(15, 8))
+ax = fig.add_subplot(111)
+ax.set_aspect("equal")
+ax.set_xlim(-5, 37)
+ax.set_ylim(30, 46)
+step = .1
+t0, t1 = a.period
+g = a.grid_count(((-5, 37, step), (30, 46, step)), center=True)
+g.vars['count'] = g.vars['count'] / (step ** 2 * (t1 - t0))
+m = g.display(ax, name='count', vmin=0, vmax=2)
+ax.grid()
+cb = plt.colorbar(m)
+cb.set_label('Eddies by 1°^2 by day')
+
---
+++ examples/pet_display_id.py
@@ -0,0 +1,22 @@
+"""
+Display identification
+======================
+
+"""
+
+from matplotlib import pyplot as plt
+from py_eddy_tracker.observations.observation import EddiesObservations
+from py_eddy_tracker import data
+
+a = EddiesObservations.load_file(data.get_path("Anticyclonic_20190223.nc"))
+c = EddiesObservations.load_file(data.get_path("Cyclonic_20190223.nc"))
+
+# Plot
+fig = plt.figure(figsize=(15,8))
+ax = fig.add_subplot(111)
+ax.set_aspect('equal')
+ax.set_xlim(0, 360)
+ax.set_ylim(-80, 80)
+a.display(ax, label="Anticyclonic", color="r", lw=1)
+c.display(ax, label="Cyclonic", color="b", lw=1)
+ax.legend(loc="upper right")
