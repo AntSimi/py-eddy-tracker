6620604587d8413119a74ba7dcf7b42008fbbfa4
Cori Pegliasco <cpegliasco@groupcls.com>
2020-11-19 17:01:04 +0100

    add test for lifetime (item)

--- tests/test_track.py
+++ tests/test_track.py
@@ -45,3 +45,6 @@ def test_area_tracking_parameter():
         c.to_netcdf(h)
         c_reloaded = Correspondances.from_netcdf(h)
         assert class_kw == c_reloaded.class_kw
+
+    # test access to the lifetime (item)
+    eddies_tracked["lifetime"]
