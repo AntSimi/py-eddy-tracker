10f42cb5e5f640bc439e73f23206fb4f9ef381bc
A. Delepoulle <36040805+AntSimi@users.noreply.github.com>
2022-10-04 09:57:07 +0200

    Add class store

--- src/py_eddy_tracker/observations/__init__.py
+++ src/py_eddy_tracker/observations/__init__.py
@@ -1,3 +1,46 @@
 # -*- coding: utf-8 -*-
 """
 """
+
+class Store:
+    def __init__(self):
+        self.store = None
+
+    def from_netcdf(self):
+        raise Exception()
+
+    def from_zarr(self):
+        raise Exception()
+
+    def to_netcdf(self):
+        raise Exception()
+
+    def to_zarr(self):
+        raise Exception()
+
+    def __getitem__(self, item):
+        if isinstance(item, slice):
+            raise Exception()
+        else:
+            return self.store[item]
+
+    def __len__(self):
+        return self.store.size
+
+    def __eq__(self, other):
+        raise Exception()
+    
+    def __iter__(self):
+        raise Exception()
+    
+    def reset(self):
+        raise Exception()
+
+    def fields(self):
+        raise Exception()
+
+class ZarrMemoryStore(Store):
+    pass
+
+class RecordArrayStore(Store):
+    pass
---
+++ tests/test_store.py
@@ -0,0 +1,19 @@
+from py_eddy_tracker.observations import ZarrMemoryStore, RecordArrayStore
+
+def test_add_field():
+    pass
+
+def test_slicing():
+    pass
+
+def test_indexing():
+    pass
+
+def test_field():
+    pass
+
+def test_size():
+    pass
+
+def test_reordering():
+    pass
