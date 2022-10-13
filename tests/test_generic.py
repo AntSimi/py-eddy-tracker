1b2c4c78c572384042a16ddd86791e331727a8e2
A. Delepoulle <36040805+AntSimi@users.noreply.github.com>
2022-10-13 10:47:53 +0200

    Add wrapping longitude test

--- tests/test_generic.py
+++ tests/test_generic.py
@@ -1,6 +1,6 @@
 from numpy import arange, array, nan, ones, zeros
 
-from py_eddy_tracker.generic import cumsum_by_track, simplify
+from py_eddy_tracker.generic import cumsum_by_track, simplify, wrap_longitude
 
 
 def test_simplify():
@@ -30,3 +30,22 @@ def test_cumsum_by_track():
     a = ones(10, dtype="i4") * 2
     track = array([1, 1, 2, 2, 2, 2, 44, 44, 44, 48])
     assert (cumsum_by_track(a, track) == [2, 4, 2, 4, 6, 8, 2, 4, 6, 2]).all()
+
+
+def test_wrapping():
+    y = x = arange(-5,5, dtype='f4')
+    x_, _ = wrap_longitude(x, y, ref=-10)
+    assert (x_ == x).all()
+    x_, _ = wrap_longitude(x, y, ref=1)
+    assert x.size == x_.size
+    assert (x_[6:] == x[6:]).all()
+    assert (x_[:6] == x[:6] + 360).all()
+    x_, _ = wrap_longitude(x, y, ref=1, cut=True)
+    assert x.size + 3 == x_.size
+    assert (x_[6 + 3:] == x[6:]).all()
+    assert (x_[:7] == x[:7] + 360).all()
+
+    # FIXME Need evolution in wrap_longitude
+    # x %= 360
+    # x_, _ = wrap_longitude(x, y, ref=-10, cut=True)
+    # assert x.size == x_.size
